c68417514fa3a1206e925e8d7f715f4ef82ac6b5
kabore safiatou <ksafiatou604@gmail.com>
2022-06-15 19:39:06 +0000

    auth

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
-require("./bootstrap");
-import "admin-lte/plugins/jquery/jquery.min";
-import "admin-lte/plugins/bootstrap/js/bootstrap.bundle.min";
-import "admin-lte/dist/js/adminlte.min";
-import "admin-lte/plugins/datatables/jquery.dataTables.js";
-require("./index")
+import './bootstrap';
+
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('etudiant');
+    return view('welcome');
 });
-Route::get('/secretaire', function () {
-    return view('secretaire');
-})->name('secretaire');
-Route::get('/carte', function () {
-    return view('carte');
-})->name('carte');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
