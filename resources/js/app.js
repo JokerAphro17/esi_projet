3a5abfd15c6319c1309c218355d4c4d674143d49
Ilboudo Souleymane <ilboudosouleymane4@gmail.com>
2022-06-16 01:49:53 +0000

    authentification with breeze

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,4 +3,10 @@ import "admin-lte/plugins/jquery/jquery.min";
 import "admin-lte/plugins/bootstrap/js/bootstrap.bundle.min";
 import "admin-lte/dist/js/adminlte.min";
 import "admin-lte/plugins/datatables/jquery.dataTables.js";
-require("./index")
+require("./index");
+
+import Alpine from "alpinejs";
+
+window.Alpine = Alpine;
+
+Alpine.start();
